cd5fbe31f8d5a5b2c6854af4134cb4527a93a89a
sonangrai <shonahangrae@gmail.com>
2025-08-28 20:38:43 +0545

    feat: loop adjustment

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,6 +2,7 @@ import SanityImageComp from "@/components/common/sanity-image";
 import { client } from "@/sanity/client";
 import { SanityDocument } from "next-sanity";
 import { format } from "date-fns";
+import Link from "next/link";
 
 const POSTS_QUERY = `*[
   _type == "post"
@@ -15,17 +16,19 @@ export default async function Home() {
   console.log(posts);
 
   return (
-    <main className="container mx-auto py-6">
-      <div className="grid gap-2 grid-cols-3">
+    <main className="container mx-auto py-6 min-h-[calc(100dvh-93px)] flex flex-col justify-center">
+      <div className="grid gap-4 grid-cols-3">
         {posts.map((post) => (
           <article className="" key={post._id}>
-            <SanityImageComp
-              asset={post.mainImage}
-              width={100}
-              height={100}
-              alt={post.title}
-              className="w-full"
-            />
+            <Link href={`/blog/${post.slug.current}`}>
+              <SanityImageComp
+                asset={post.mainImage}
+                width={100}
+                height={100}
+                alt={post.title}
+                className="w-full h-[240px] object-cover"
+              />
+            </Link>
             <h2 className="font-bold my-2">{post.title}</h2>
             <span className="text-xs">
               {format(post.publishedAt, "MMM d, y")}
