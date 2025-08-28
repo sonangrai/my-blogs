f68d2f43f39f5d14c78b10eb2ffbb6a39be7ff3c
sonangrai <shonahangrae@gmail.com>
2025-08-28 20:20:17 +0545

    feat: lopped blogs

--- frontend/src/sanity/client.ts
+++ frontend/src/sanity/client.ts
@@ -1,4 +1,6 @@
 import { createClient } from "next-sanity";
+import imageUrlBuilder from "@sanity/image-url";
+import { SanityImageSource } from "@sanity/image-url/lib/types/types";
 
 export const client = createClient({
   projectId: process.env.PROJECT_ID,
@@ -6,3 +8,31 @@ export const client = createClient({
   apiVersion: "2024-01-01",
   useCdn: false,
 });
+
+const builder = imageUrlBuilder(client);
+
+export function urlFor(source: SanityImageSource) {
+  return builder.image(source);
+}
+
+export interface SanityImage {
+  _type: "image";
+  asset: {
+    _ref: string;
+    _type: "reference";
+  };
+  alt?: string;
+  caption?: string;
+}
+
+export interface SanityImageAsset {
+  _id: string;
+  url: string;
+  metadata: {
+    dimensions: {
+      width: number;
+      height: number;
+      aspectRatio: number;
+    };
+  };
+}
