f1879a4055d8a0da5c6c47d1d183718f9e082028
sonangrai <shonahangrae@gmail.com>
2025-08-31 12:07:57 +0545

    feat: added ads sense script

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -28,6 +28,13 @@ export default function RootLayout({
   return (
     <ThemeProvider>
       <html lang="en">
+        <head>
+          <script
+            async
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9818673900351802"
+            crossOrigin="anonymous"
+          ></script>
+        </head>
         <body
           className={`${geistSans.variable} ${hubotSans.variable} antialiased`}
         >
